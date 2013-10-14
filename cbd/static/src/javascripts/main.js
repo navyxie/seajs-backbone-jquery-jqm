0a7f6ec1cba99d20ce620b5c1b3cadfe779e7932
navy <navyxie2010@gmail.com>
2013-10-14 22:00:59 +0800

    添加template

--- cbd/static/src/javascripts/main.js
+++ cbd/static/src/javascripts/main.js
@@ -1,10 +1,14 @@
 define(function(require){
 	var $ = require('jquery');
 	var jqmobile = require('jqmobile');
-	var underscore = require('underscore');
-	var backbone = require('backbone');
-	console.log($);
-	console.log(jqmobile);
-	console.log(backbone);
-	console.log(underscore);
+	// var underscore = require('underscore');
+	// var backbone = require('backbone');
+	// console.log($);
+	// console.log(jqmobile);
+	// console.log(backbone);
+	// console.log(underscore);
+	$(document).on('pageinit',function(){
+		console.log(jqmobile.ajaxEnabled);
+	})
+
 });
